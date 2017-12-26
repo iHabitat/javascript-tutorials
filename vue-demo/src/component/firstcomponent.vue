3aff16b0e90edca99d4611778d198d2c1631fd46
ThomsonTang <guiketang@gmail.com>
2017-12-26 21:39:44 +0800

    vue component.

---
+++ vue-demo/src/component/firstcomponent.vue
@@ -0,0 +1,13 @@
+<div id="example">
+    <first-component></first-component>
+</div>
+
+<script>
+    Vue.component('first-component', {
+        template: '<div>This is a custom component.</div>'
+    })
+
+    new Vue({
+        el: '#example'
+    })
+</script>
