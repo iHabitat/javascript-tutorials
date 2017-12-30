8ec8d271c418330e9213a528b4c247801bef0d52
ThomsonTang <guiketang@gmail.com>
2017-12-30 10:57:38 +0800

    add the vue router app.

---
+++ vue-router-app/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <h1>Hello App!</h1>
+    <p>
+      <!--使用 router-link 组件来导航，通过传入 `to` 属性指定链接。<router-link> 默认会被渲染成一个 `<a>` 标签-->
+      <router-link to="/first">Go to First</router-link>
+      <router-link to="/second">Go to Second</router-link>
+    </p>
+    <!-- 路由出口，路由匹配到的组件将渲染在这里 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+      };
+    },
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
