c6ce8f8458351653264204cb4142342c6034dfa9
ThomsonTang <guiketang@gmail.com>
2018-07-12 23:22:50 +0800

    DEV-CM: config the babel and finish a simple example of vue component.

--- webpack-demo/main.js
+++ webpack-demo/main.js
@@ -1,2 +1,10 @@
-import './style.css'
-document.getElementById('app').innerHTML = 'Hello Webpack.';
+// import vue framework.
+import Vue from 'vue';
+
+// import app.vue component
+import App from './app.vue';
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+});
